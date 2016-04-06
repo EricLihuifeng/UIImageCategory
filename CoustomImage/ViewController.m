856478753286513ad834907f6a920f29e6e24752
YC L <yc_l@yc-ldemacbook-pro.local>
2016-04-06 16:08:33 +0800

    Initial Commit

---
+++ CoustomImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CoustomImage
+//
+//  Created by YC_L on 16/4/6.
+//  Copyright © 2016年 LiHF. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CoustomImage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoustomImage
+//
+//  Created by YC_L on 16/4/6.
+//  Copyright © 2016年 LiHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
