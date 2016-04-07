8de68e5bd46fe6ee657a4b5e40eef196c6fc54ff
YC_L <yc_l@YC-LdeMacBook-Pro.local>
2016-04-07 18:09:39 +0800

    first commit

---
+++ CoustomImage/UIImage+SYUIImageCategory.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+SYUIImageCategory.m
+//  CoustomImage
+//
+//  Created by YC_L on 16/4/6.
+//  Copyright © 2016年 LiHF. All rights reserved.
+//
+
+#import "UIImage+SYUIImageCategory.h"
+
+@implementation UIImage (SYUIImageCategory)
+
+@end
